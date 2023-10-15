24c004eb77c591796daf4b10bc14f7ccc7c52eb3
JJay405 <111616452+JJay405@users.noreply.github.com>
2023-10-15 11:43:55 -0700

    Create StackDemo.java

---
+++ DataStructure/StackAndQueue/StackDemo.java
@@ -0,0 +1,35 @@
+//Junhong Liu
+
+public class StackDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		ResizingArrayStack<String>  strStack= new ResizingArrayStack<>(10);
+		
+		ResizingArrayStack<Character>  charStack= new ResizingArrayStack<>(10);
+		
+		String str="Math140";
+		String str2 = "";
+		
+		for( int i = 0; i <str.length();i++) {
+			
+			charStack.push(str.charAt(i));
+			
+		}
+		
+		while((!charStack.isEmpty())) {
+			
+			
+				str2=str2+charStack.pop();
+				
+			
+		}
+		
+		
+		
+		System.out.println(str2);
+	}
+
+}
